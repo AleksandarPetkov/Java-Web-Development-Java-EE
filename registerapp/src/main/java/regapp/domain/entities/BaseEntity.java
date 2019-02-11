121b8b590e3d48ccb96d388bf2161cf707447415
AleksandarPetkov <A.PetkovPetkov@gmail.com>
2019-02-11 10:52:25 +0200

    Add Register App Configuration

---
+++ registerapp/src/main/java/regapp/domain/entities/BaseEntity.java
@@ -0,0 +1,23 @@
+package regapp.domain.entities;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+
+    private String id;
+
+    public BaseEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
