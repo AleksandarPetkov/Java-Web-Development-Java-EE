8d58fd704a9e07df2428932026c4ecf9c8cacee3
AleksandarPetkov <A.PetkovPetkov@gmail.com>
2019-02-15 13:17:25 +0200

    Add Cat Repository Add Cat Service Add Cat Binding Model

---
+++ fluffyduffymunchingcatV2/src/main/java/app/domain/binding/CreateCatBindingModel.java
@@ -0,0 +1,90 @@
+package app.domain.binding;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class CreateCatBindingModel {
+
+    private String name;
+
+    private String breed;
+
+    private String color;
+
+    private int age;
+
+    private String genger;
+
+    private BigDecimal price;
+
+    private LocalDate addedOn;
+
+    private boolean hasPassport;
+
+    public CreateCatBindingModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGenger() {
+        return genger;
+    }
+
+    public void setGenger(String genger) {
+        this.genger = genger;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getAddedOn() {
+        return addedOn;
+    }
+
+    public void setAddedOn(LocalDate addedOn) {
+        this.addedOn = addedOn;
+    }
+
+    public boolean isHasPassport() {
+        return hasPassport;
+    }
+
+    public void setHasPassport(boolean hasPassport) {
+        this.hasPassport = hasPassport;
+    }
+}
---
+++ fluffyduffymunchingcatV2/src/main/java/app/web/beans/CarCreateBean.java
@@ -0,0 +1,11 @@
+package app.web.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class CarCreateBean {
+
+
+}
